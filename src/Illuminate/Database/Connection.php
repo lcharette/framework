ff25c25a1ffcdc3f7bf6a2f18c6e4af8c31ffac1
Louis Charette <charette.louis@gmail.com>
2022-03-20 16:20:13 -0400

    Close doctrineConnection on disconnect

--- src/Illuminate/Database/Connection.php
+++ src/Illuminate/Database/Connection.php
@@ -829,6 +829,7 @@ class Connection implements ConnectionInterface
     public function disconnect()
     {
         $this->setPdo(null)->setReadPdo(null);
+        $this->doctrineConnection = null;
     }
 
     /**
